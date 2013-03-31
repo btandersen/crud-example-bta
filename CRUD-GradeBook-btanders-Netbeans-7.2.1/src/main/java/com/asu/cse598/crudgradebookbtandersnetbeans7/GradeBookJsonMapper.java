2249890bae62bb876f4d75e26d5ae2d93678a45c
btanders@asu.edu <btanders@asu.edu@5ce57db6-2b37-2cc7-a9b0-c302c6f7a091>
2013-03-31 03:09:28 +0000

    update

--- CRUD-GradeBook-btanders-Netbeans-7.2.1/src/main/java/com/asu/cse598/crudgradebookbtandersnetbeans7/GradeBookJsonMapper.java
+++ CRUD-GradeBook-btanders-Netbeans-7.2.1/src/main/java/com/asu/cse598/crudgradebookbtandersnetbeans7/GradeBookJsonMapper.java
@@ -4,7 +4,9 @@
  */
 package com.asu.cse598.crudgradebookbtandersnetbeans7;
 
+import java.util.ArrayList;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
 /**
  *
@@ -23,7 +25,15 @@ public class GradeBookJsonMapper {
     }
     
     public Student studentJsonToObj(String studentJson) throws Exception {
-        return mapper.readValue(studentJson, Student.class);
+        return this.mapper.readValue(studentJson, Student.class);
+    }
+    
+    public String studentListToJson(ArrayList<Student> studentList) throws Exception {
+        return this.mapper.writeValueAsString(studentList);
+    }
+    
+    public ArrayList<Student> studentListJsonToList(String studentListJson) throws Exception {
+        return this.mapper.readValue(studentListJson, new TypeReference<ArrayList<Student>>(){});
     }
     
     public String gradedWorkItemObjToJson(GradedWorkItem gradedWorkItem) throws Exception {
@@ -31,7 +41,7 @@ public class GradeBookJsonMapper {
     }
     
     public GradedWorkItem gradedWorkItemJsonToObj(String gradedWorkItemJson) throws Exception {
-        return mapper.readValue(gradedWorkItemJson, GradedWorkItem.class);
+        return this.mapper.readValue(gradedWorkItemJson, GradedWorkItem.class);
     }
     
     public String workItemObjToJson(WorkItem workItem) throws Exception {
